447d5923752e4c4edcd18a95ed0d6a5e582b8395
Nadeesha Pathiraja <pdncpathiraja95@gmail.com>
2021-10-26 13:30:05 +0530

    Add project

---
+++ lib/login_screen/login_screen.dart
@@ -0,0 +1,27 @@
+import 'package:email_auth/utils/constants.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(
+              Constants.imageAssets("bg.jpg"),
+            ),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
