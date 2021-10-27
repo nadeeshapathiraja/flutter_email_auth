3bc202e42d227230baf843eefba000054b035e5b
Nadeesha Pathiraja <pdncpathiraja95@gmail.com>
2021-10-28 02:18:51 +0530

    Firebase Autendication set Success

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:email_auth/login_screen/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
